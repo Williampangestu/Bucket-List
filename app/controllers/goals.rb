582cd2810b38598480e3acbf8cf1f8096c05f480
William Pangestu <pangestu.william@gmail.com>
2014-09-09 19:31:12 -0700

    Create add new goal functionality Create complete goals functionality Create like functionality Create join functionality

--- app/controllers/goals.rb
+++ app/controllers/goals.rb
@@ -1,14 +1,31 @@
-post '/goals/:id/likes' do
+post '/users/:user_id/goals/:id/likes' do
   goal = Goal.find(params[:id])
   goal.update(likes: goal.likes + 1)
   content_type :json
   {likes: goal.likes}.to_json
 end
 
-post '/goals/:id/participants' do
+post '/users/:user_id/goals/:id/participants' do
   goal = Goal.find(params[:id])
-  goal.users.count += 1
-  p goal.users.count
+  user = User.find(params[:user_id])
+  goal.users << user
   content_type :json
   {participants: goal.users.count}.to_json
+end
+
+get '/users/:user_id/goals/completes' do
+  @user = User.find(params[:user_id])
+  erb :_complete_form, layout: false
+end
+
+get '/users/:user_id/newgoal' do
+  @user = User.find(params[:user_id])
+  erb :_new_form, locals: {user: @user}
+end
+
+post '/users/:user_id/goals/new' do
+  title = params[:title]
+  p params
+  content_type :json
+  {title: title}.to_json
 end
--- public/js/application.js
+++ public/js/application.js
@@ -3,17 +3,21 @@ $(document).ready(function() {
   function bindEvents() {
     $(".like").on("click", incrementLike)
     $(".join").on("click", incrementParticipant)
+    $(".fa-square-o").on("click", completeGoal)
+    $(".add").on("click", addNewForm)
   }
 
   function incrementLike(event) {
     event.preventDefault();
-    var id = $(this).parents('div').eq(1).attr('id')
+    $like = $(this)
+    var goal_id = $(this).parents('div').eq(1).attr('id')
+    var user_id = $(this).parents('div').eq(1).attr('class').split(" ")[0]
     $.ajax({
-      url: '/goals/'+id+'/likes',
+      url: '/users/' +user_id+ '/goals/' +goal_id+ '/likes',
       type: 'post'
     })
     .done(function(data){
-      debugger
+      $like.html("<i class='fa fa-thumbs-up'></i> " + data.likes)
     })
     .fail(function(){
       console.log("You Failed!")
@@ -22,13 +26,70 @@ $(document).ready(function() {
 
   function incrementParticipant(event) {
     event.preventDefault();
-    var id = $(this).parents('div').eq(1).attr('id')
+    $join = $(this)
+    var goal_id = $(this).parents('div').eq(1).attr('id')
+    var user_id = $(this).parents('div').eq(1).attr('class').split(" ")[0]
+
     $.ajax({
-      url: '/goals/'+id+'/participants',
+      url: '/users/' +user_id+ '/goals/' +goal_id+ '/participants',
       type: 'post'
     })
     .done(function(data){
-      debugger
+      $join.parent().next().text(data.participants + " People Are Going")
+    })
+    .fail(function(){
+      console.log("You Failed!")
+    })
+  }
+
+  function completeGoal(event) {
+    event.preventDefault();
+    $complete = $(this)
+    var id = $(this).parents('div').attr('id')
+    $.ajax({
+      url: '/users/' +id+ '/goals/completes',
+      type: 'get'
+    })
+    .done(function(data){
+      $complete.parent().append(data)
+      $complete.parent().find('i').eq(0).remove()
+    })
+    .fail(function(){
+      console.log("You Failed!")
+    })
+  }
+
+  function addNewForm(event) {
+    event.preventDefault();
+    $add = $(this)
+    var id = $(this).closest('div').data("id")
+    $.ajax({
+      url: '/users/' +id+ '/newgoal',
+      type: 'get'
+    })
+    .done(function(data){
+      $add.remove()
+      $('.new').append(data)
+      $('.newgoal').on("submit", addGoal)
+    })
+    .fail(function(){
+      console.log("You Failed!")
+    })
+  }
+
+  function addGoal(event) {
+    event.preventDefault();
+    var id = $(this).data("id")
+    $.ajax({
+      url: "/users/" +id+ "/goals/new",
+      type: "post",
+      data: $(this).serialize()
+    })
+    .done(function(data){
+      var template = "<li><i class='fa fa-square-o'></i>&nbsp " +data.title + "</li>"
+      $('.goals').find('ul').append(template)
+      $('.newgoal').remove()
+      $('.new').append("<button class='add'>Add Goal</button>")
     })
     .fail(function(){
       console.log("You Failed!")
